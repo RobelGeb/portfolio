67b49121a238f7c238bd3db88848161820240923
RobelGeb <r.gebremichael@outlook.com>
2025-03-21 01:31:13 -0700

    radial background

--- app/layout.tsx
+++ app/layout.tsx
@@ -35,7 +35,7 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <body
-        className={`${geist.variable} ${geistMono.variable} bg-white tracking-tight antialiased dark:bg-zinc-950`}
+        className={`${geist.variable} ${geistMono.variable} bg-radial from-white to-blue-200 dark:from-stone-900 dark:to-blue-950  tracking-tight antialiased dark:bg-zinc-950`}
       >
         <ThemeProvider
           enableSystem={true}
@@ -43,15 +43,32 @@ export default function RootLayout({
           storageKey="theme"
           defaultTheme="system"
         >
-          <div className="flex min-h-screen w-full flex-col font-[family-name:var(--font-inter-tight)]">
-            <div className="relative mx-auto w-full max-w-screen-sm flex-1 pt-20">
-              <Header />
-              {children}
-              <Footer />
-            </div>
-          </div>
+              <div className="relative mx-auto w-full max-w-screen-sm flex-1 pt-20 ">
+                <Header />
+                {children}
+                <Footer />
+              </div>
         </ThemeProvider>
       </body>
     </html>
   )
 }
+
+{/* light dot grid */}
+{/* <div class="relative h-full w-full bg-white">
+  <div class="absolute h-full w-full bg-[radial-gradient(#e5e7eb_1px,transparent_1px)] [background-size:16px_16px] [mask-image:radial-gradient(ellipse_50%_50%_at_50%_50%,#000_70%,transparent_100%)]">
+  </div>
+</div> */}
+
+{/* dark line grid */}
+{/* <div class="relative h-full w-full bg-slate-950">
+  <div class="absolute bottom-0 left-0 right-0 top-0 bg-[linear-gradient(to_right,#4f4f4f2e_1px,transparent_1px),linear-gradient(to_bottom,#4f4f4f2e_1px,transparent_1px)] bg-[size:14px_24px] [mask-image:radial-gradient(ellipse_60%_50%_at_50%_0%,#000_70%,transparent_100%)]">
+  </div>
+</div> */}
+
+{/* <div class="relative h-full w-full bg-slate-950">
+  <div class="absolute bottom-0 left-[-20%] right-0 top-[-10%] h-[500px] w-[500px] rounded-full bg-[radial-gradient(circle_farthest-side,rgba(255,0,182,.15),rgba(255,255,255,0))]"></div>
+  <div class="absolute bottom-0 right-[-20%] top-[-10%] h-[500px] w-[500px] rounded-full bg-[radial-gradient(circle_farthest-side,rgba(255,0,182,.15),rgba(255,255,255,0))]">
+  </div>
+  </div>
+  */}
